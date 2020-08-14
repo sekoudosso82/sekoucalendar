697f4ee4051bf1c39c0d25cbfba9dfdaaab59a81
sekoudosso82 <sekou.dosso82@gmail.com>
2020-08-14 01:10:47 -0400

    front end deployment on github

---
+++ my-to-do-list/build/precache-manifest.561309b5b07dad1325d7bf222e3decf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418ce7e8d472a63e86d03c29ac4d113e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "406e1cf561c38333919e",
+    "url": "/static/css/main.805ef6a3.chunk.css"
+  },
+  {
+    "revision": "705dd4cd358ce57aa956",
+    "url": "/static/js/2.f97aaba6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f97aaba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406e1cf561c38333919e",
+    "url": "/static/js/main.720cdd5b.chunk.js"
+  },
+  {
+    "revision": "0df70482deebec6e443e",
+    "url": "/static/js/runtime-main.51f64c33.js"
+  }
+]);
