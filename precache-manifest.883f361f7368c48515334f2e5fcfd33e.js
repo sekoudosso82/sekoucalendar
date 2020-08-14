78648b257bedf1263156286801d65de9ca6b23fb
sekoudosso82 <sekou.dosso82@gmail.com>
2020-08-14 01:15:34 -0400

    Updates

---
+++ precache-manifest.883f361f7368c48515334f2e5fcfd33e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffe631625c3d24e718ba278c0091b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a42ca17451508f14bb5",
+    "url": "/static/css/main.805ef6a3.chunk.css"
+  },
+  {
+    "revision": "97bf2b3a7d40eaf168c1",
+    "url": "/static/js/2.cbfa7b5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbfa7b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a42ca17451508f14bb5",
+    "url": "/static/js/main.024d3c70.chunk.js"
+  },
+  {
+    "revision": "eb2ef73aaae7e6c5e4f3",
+    "url": "/static/js/runtime-main.76979e5e.js"
+  }
+]);
